c0b8a711063a1e6046eb1b74a3fde6c3102a2d35
alberto-fsg <albertofranciscosolazgarcia@gmail.com>
2025-02-26 12:31:01 +0100

    Borrar practica1 porque tiene el nombre mal puesto

--- practica1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lfrwp_init.a -l"D:/Mi\ unidad/SEI_2022_WS/Modulos_PR/Debug/Modulos_PR.lib" -llibc.a
-
